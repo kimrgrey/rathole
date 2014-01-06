c6f2b87146572eb9782bc7309a374292cbe365c7
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2014-01-06 14:38:06 +0400

    Issue #42: Settings tab added to the account's page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,20 @@ class UsersController < ApplicationController
     @sections = @user.sections
   end
 
+  def update
+    @user.attributes = user_params
+    if @user.save
+      invalidate_user_cache(@user)
+      flash[:notice] = I18n.t('users.update.success')
+      redirect_to user_path
+    else
+      flash[:error] = I18n.t('users.update.failed')
+      @posts = @user.last_posts
+      @sections = @user.sections
+      render :show
+    end
+  end
+
   def avatar
     if params[:file].present?
       @user.avatar = open(params[:file], 'rb')
@@ -23,6 +37,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 
   def load_user
     @user = current_user
