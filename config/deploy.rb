9d84467314179768136075ebbeec7f6859761b9c
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2014-01-21 21:23:27 +0400

    Fixed problem with sharing of uploaded files.

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ set :deploy_to, '/home/rathole/rathole'
 set :deploy_via, :remote_cache
 set :log_level, :debug
 set :keep_releases, 5
+set :linked_dirs, fetch(:linked_dirs) + %w{public/uploads}
 set :linked_files, %w{config/database.yml config/secrets.yml}
 
 namespace :deploy do
