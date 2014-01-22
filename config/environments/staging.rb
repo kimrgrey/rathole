36213beba563cedfd7e23a254483e0ae9e3dc684
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2014-01-22 21:26:07 +0400

    Host staging.rathole.io is used as default url for mailer in staging.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -12,7 +12,7 @@ Kgblogs::Application.configure do
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = { :host => 'rathole.io' }
+  config.action_mailer.default_url_options = { :host => 'staging.rathole.io' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.mandrillapp.com",
