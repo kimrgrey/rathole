a5f3b606caf290b491fa2010c7492ad1d14f1058
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2014-01-27 20:02:19 +0400

    Issue #45: Moscow time zone is used by default.

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ Bundler.require(:default, Rails.env)
 module Kgblogs
   class Application < Rails::Application
     config.i18n.default_locale = :ru
+    config.time_zone = 'Moscow'
     config.after_initialize do
       ActionView::Base.sanitized_allowed_attributes += ['rel', 'target']
     end
