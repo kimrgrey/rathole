8050dfd1e04f6b886edc841680f25c31e1bcd993
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2014-01-30 00:50:41 +0400

    Issue #25: Scroll to top should not be visible on small devices.

--- app/assets/javascripts/scroll_to_top.js
+++ app/assets/javascripts/scroll_to_top.js
@@ -1,18 +1,21 @@
 function initBackToTop() {
-  var link = $('#scroll-to-top')
+  var link = $('#scroll-to-top');
+  
   link.tooltip();
+
   link.click(function() {
     $("html, body").animate({ scrollTop: 0 }, 1000);
     return false;
   });
 
   $(window).scroll(function() {
-    if ($(this).scrollTop()) {
-      $('#scroll-to-top:hidden').stop(true, true).fadeIn();
+    if ($(this).scrollTop() > $(window).height() && link.css('z-index') > 0) {
+      link.fadeIn();
     } else {
-      $('#scroll-to-top').stop(true, true).fadeOut();
+      link.fadeOut();
     }
   });
+  
 }
 
 $(document).ready(initBackToTop);
