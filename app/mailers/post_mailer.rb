c97efa7d54b1914d25969b4871f67fad61061962
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2014-01-29 18:56:16 +0400

    Issue #77: Author of the post should not receive mails about his activity in this post.

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -9,13 +9,13 @@ class PostMailer < ActionMailer::Base
     @post = comment.post
     @author = comment.user
     @user = @post.user
-    mail(to: @user.email, subject: I18n.t('post_mailer.new_comment_created.subject'))
+    mail(to: @user.email, subject: I18n.t('post_mailer.new_comment_created.subject')) unless @author == @user
   end
 
   def new_post_created(post, admin)
     @post = post
     @author = post.user
     @user = admin
-    mail(to: @user.email, subject: I18n.t('post_mailer.new_post_created.subject')) 
+    mail(to: @user.email, subject: I18n.t('post_mailer.new_post_created.subject')) unless @author == @admin
   end
 end
