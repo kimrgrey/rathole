424748a4426540f79cc03ae6c4f4fe562ef47a43
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2014-02-16 00:51:06 +0400

    All sensitive data was removed from version control

--- config/deploy/production.rb
+++
@@ -1,4 +0,0 @@
-server 'rathole.io', user: 'rathole', roles: %w{web app db}
-
-set :stage, :production
-set :branch, 'production'
--- config/deploy/staging.rb
+++
@@ -1,4 +0,0 @@
-server 'staging.rathole.io', user: 'rathole', roles: %w{web app db}
-
-set :stage, :staging
-set :branch, 'master'
