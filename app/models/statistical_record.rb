d846d65a58bcfdbe6bd346f45ce72a828103c1c3
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2014-02-21 04:24:56 +0400

    Bugfixing in statistical system: data should be grouped by date.

--- app/models/statistical_record.rb
+++ app/models/statistical_record.rb
@@ -1,15 +1,18 @@
 class StatisticalRecord < ActiveRecord::Base
   validates :date, presence: true, uniqueness: true
+  validates :comments_count, presence: true
   validates :users_count, presence: true
   validates :posts_count, presence: true
   validates :events_count, presence: true
 
-  def self.collect_and_save!
+  def self.collect_and_save!(date = nil)
+    date ||= Time.zone.today - 1.day
     record = StatisticalRecord.new
-    record.users_count = User.count
-    record.posts_count = Post.count
-    record.events_count = Events::Event.count
-    record.date = Date.today
+    record.users_count = User.where('users.created_at::date = ?', date).count
+    record.posts_count = Post.where('posts.created_at::date = ?', date).count
+    record.comments_count = Comment.where('comments.created_at::date = ?', date).count
+    record.events_count = Events::Event.where('events.created_at::date = ?', date).count
+    record.date = date
     record.save!
   end
 end
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 set :output, '/home/rathole/rathole/shared/log/whenever.log'
 
-every 1.day, at: '4:00 am', roles: [:app] do
+every 1.day, at: '4:30 am', roles: [:app] do
   rake 'stickers:distribute'
   rake 'rathole:collect_statistic'
 end
