3529568f52fc850c3c5c53898dc3cae46bdea54c
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2014-02-25 20:29:29 +0400

    Admin's dashboard was implemented using tabs.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,10 +4,10 @@ class Admin::DashboardController < Admin::AdminController
   def home
     @users = User.all
     @users = @users.order(:created_at => :desc)
-    @users = @users.page(params[:page]).per(params[:per])
+    @users = @users.page(params[:users_page]).per(params[:per])
     @posts = Post.all
     @posts = @posts.order(:created_at => :desc)
-    @posts = @posts.page(params[:page]).per(params[:per])
+    @posts = @posts.page(params[:posts_page]).per(params[:per])
   end
 
   def statistics
