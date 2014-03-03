4ad56a5df8d8b6759c025a1798423abd0798918b
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2014-03-03 19:57:46 +0400

    Initialization of flot should be performed only for admin page.

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,17 +1,17 @@
 (function(){
   function initDashboard() {
     var dashboard = $('.admin .dashboard');
-
-    dashboard.find('tbody tr').click(function(event){
-      event.preventDefault();
-      window.location.href = $(this).attr('data-url');
-      return false;
-    });
-    var placeholder = $("#statistics-flot-placeholder");
-    $.get(placeholder.attr('data-url'), function(response){
-      $.plot(placeholder, response.values, response.options);
-    });
-
+    if (dashboard.length > 0) {
+      dashboard.find('tbody tr').click(function(event){
+        event.preventDefault();
+        window.location.href = $(this).attr('data-url');
+        return false;
+      });
+      var placeholder = $("#statistics-flot-placeholder");
+      $.get(placeholder.attr('data-url'), function(response){
+        $.plot(placeholder, response.values, response.options);
+      });
+    }
   }
 
   $(document).ready(initDashboard);
