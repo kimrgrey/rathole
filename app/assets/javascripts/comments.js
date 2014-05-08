e6d874317d034c2d173b1e63a1307f2829890035
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2014-05-08 18:42:34 +0400

    Reply link was added to the comment's view

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -17,6 +17,24 @@
       $('#new-comment-form').show();
       return false;
     });
+
+    $('.comments').on('click', '.reply-comment-link', function(event){
+      event.preventDefault();
+      var author = $(this).attr('data-author');
+      var textArea = $('.comments #new-comment-form #body');
+      var commentText = textArea.val();
+      if (commentText.length > 0) {
+        commentText += "\r\n\r\n";
+      }
+      commentText += "**" + author + "**";
+      var selectedText = getSelection().toString();
+      if (selectedText.length > 0) {
+        commentText += "\r\n\r\n" + "> " + selectedText;
+      }
+      textArea.val(commentText);
+      $('html, body').animate({ scrollTop: textArea.offset().top - 200}, 500);
+      return false;
+    });
   }
 
   $(document).ready(initComments);
