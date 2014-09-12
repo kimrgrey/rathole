762c0025d4e388a001b3ee3147f0c61750103f65
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2014-09-12 22:55:37 +0400

    Bump schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140530153948) do
+ActiveRecord::Schema.define(version: 20140912184019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -171,7 +171,7 @@ ActiveRecord::Schema.define(version: 20140530153948) do
     t.datetime "locked_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "user_name"
+    t.string   "user_name",                           null: false
     t.integer  "posts_count",            default: 0
     t.integer  "comments_count",         default: 0
     t.string   "avatar"
@@ -186,5 +186,6 @@ ActiveRecord::Schema.define(version: 20140530153948) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
+  add_index "users", ["user_name"], name: "index_users_on_user_name", unique: true, using: :btree
 
 end
