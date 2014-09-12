4fabccf4241641f672acf58d5082bbaf965c4d27
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2014-09-13 02:20:45 +0400

    Implemented admin's dashboard using setup event

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,18 +1,18 @@
 (function(){
-  function initDashboard() {
-    var dashboard = $('.admin .dashboard');
-    if (dashboard.length > 0) {
-      dashboard.find('tbody tr').click(function(event){
-        event.preventDefault();
-        window.location.href = $(this).attr('data-url');
-        return false;
-      });
-      var placeholder = $("#statistics-flot-placeholder");
-      $.get(placeholder.attr('data-url'), function(response){
-        $.plot(placeholder, response.values, response.options);
-      });
-    }
-  }
+  $(document).on('setup', '.admin .dashboard', function(){
+    var $dashboard = $(this);
+    $dashboard.find('tbody tr').click(function(event){
+      event.preventDefault();
+      window.location.href = $(this).attr('data-url');
+      return false;
+    });
+    var $placeholder = $("#statistics-flot-placeholder");
+    $.get($placeholder.attr('data-url'), function(response){
+      $.plot($placeholder, response.values, response.options);
+    });
+  });
 
-  $(document).ready(initDashboard);
+  $(document).ready(function(){
+    $('.admin .dashboard').trigger('setup');
+  });
 })();
