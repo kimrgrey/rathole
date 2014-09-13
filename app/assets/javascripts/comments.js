db7b8a1824f38ed0c3c309de0bb4fb90fc6adb2c
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2014-09-13 18:11:22 +0400

    Implemented comments using setup event

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,28 +1,32 @@
 (function() {
-  function initComments() {
-    $('.comments').on('click', '.edit-comment-link', function(event){
+  $(document).on('setup', '.comments', function(event){
+    event.stopPropagation();
+
+    var $comments = $(this);
+
+    $(this).on('click', '.edit-comment-link', function(event){
       event.preventDefault();
-      var comment = $($(this).attr('href'));
-      comment.find('.text').hide();
-      comment.find('form').addClass('visible');
+      var $comment = $($(this).attr('href'));
+      $comment.find('.text').hide();
+      $comment.find('form').addClass('visible');
       $('#new-comment-form').hide();
       return false;
     });
 
-    $('.comments').on('click', '.comment-edit-cancel', function(event){
+    $(this).on('click', '.comment-edit-cancel', function(event){
       event.preventDefault();
-      var comment = $($(this).attr('href'));
-      comment.find('form').removeClass('visible');
-      comment.find('.text').show();
+      var $comment = $($(this).attr('href'));
+      $comment.find('form').removeClass('visible');
+      $comment.find('.text').show();
       $('#new-comment-form').show();
       return false;
     });
 
-    $('.comments').on('click', '.reply-comment-link', function(event){
+    $(this).on('click', '.reply-comment-link', function(event){
       event.preventDefault();
       var author = $(this).attr('data-author');
-      var textArea = $('.comments #new-comment-form #body');
-      var commentText = textArea.val();
+      var $textArea = $comments.find('#new-comment-form #body');
+      var commentText = $textArea.val();
       if (commentText.length > 0) {
         commentText += "\r\n\r\n";
       }
@@ -31,11 +35,13 @@
       if (selectedText.length > 0) {
         commentText += "\r\n\r\n" + "> " + selectedText;
       }
-      textArea.val(commentText);
-      $('html, body').animate({ scrollTop: textArea.offset().top - 200}, 500);
+      $textArea.val(commentText);
+      $('html, body').animate({ scrollTop: $textArea.offset().top - 200}, 500);
       return false;
     });
-  }
+  });
 
-  $(document).ready(initComments);
+  $(document).ready(function() {
+    $('.comments').trigger('setup');
+  });
 })();
