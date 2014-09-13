3e6cba213f4ffc2663ba3eebb85db0ec8d2eb2af
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2014-09-13 18:16:48 +0400

    Implemented post's wall using setup event

--- app/assets/javascripts/posts-wall.js
+++ app/assets/javascripts/posts-wall.js
@@ -1,11 +1,16 @@
-$(document).ready(function(){
-  var wall = $('#posts-wall');
-  if (wall.length > 0) {
+(function(){
+  $(document).on('setup', '#posts-wall', function(){
+    var $wall = $(this);  
     var options = {
       itemSelector: '.posts-wall-item',
       isFitWidth: true,
       gutter: 30
     };
-    wall.masonry(options);
-  }
-});
+    $wall.masonry(options);
+  });
+
+  $(document).ready(function(){
+    $('#posts-wall').trigger('setup');  
+  });  
+})();
+
