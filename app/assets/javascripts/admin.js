ef3958ae6d808e52b9453ac10b944294a284b02b
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2014-09-30 21:13:57 +0400

    Used links instead of click in admin's tables. Close #54.

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -2,11 +2,6 @@
   $(document).on('setup', '.admin .dashboard', function(event){
     event.stopPropagation();
     var $dashboard = $(this);
-    $dashboard.find('tbody tr').click(function(event){
-      event.preventDefault();
-      window.location.href = $(this).attr('data-url');
-      return false;
-    });
     var $placeholder = $("#statistics-flot-placeholder");
     $.get($placeholder.attr('data-url'), function(response){
       $.plot($placeholder, response.values, response.options);
