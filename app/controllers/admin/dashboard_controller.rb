521c9da2495afd4f26222b0742f6935516ec8300
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2014-10-21 22:26:37 +0400

    Displayed claims on admin's dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -8,6 +8,9 @@ class Admin::DashboardController < Admin::AdminController
     @posts = Post.all
     @posts = @posts.order(:created_at => :desc)
     @posts = @posts.page(params[:posts_page]).per(params[:per])
+    @claims = Claim.all
+    @claims = @claims.in_order
+    @claims = @claims.page(params[:claims_page]).per(params[:per])
   end
 
   def statistics
--- app/models/claim.rb
+++ app/models/claim.rb
@@ -2,4 +2,6 @@ class Claim < ActiveRecord::Base
   belongs_to :post, touch: true
 
   validates :post, presence: true
+
+  scope :in_order, -> { order('claims.created_at DESC') }
 end
