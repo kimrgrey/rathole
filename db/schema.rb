048d2dec685168fec14fc620f6e5e2a48a52f28a
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2014-10-25 19:28:21 +0400

    Bump schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024174030) do
+ActiveRecord::Schema.define(version: 20141025150106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,10 @@ ActiveRecord::Schema.define(version: 20141024174030) do
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
   end
+
+  add_index "claims", ["email"], name: "index_claims_on_email", using: :btree
 
   create_table "comments", force: true do |t|
     t.text     "body"
