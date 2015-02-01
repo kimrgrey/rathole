6e299570a632912422282e54315632194b12c066
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2015-02-01 03:17:03 +0300

    Removed all authorizers

--- app/authorizers/post_authorizer.rb
+++
@@ -1,9 +0,0 @@
-class PostAuthorizer < BaseAuthorizer
-  def self.readable_by?(user)
-    true
-  end
-
-  def readable_by?(user)
-    resource.published? || user.is_author_of?(resource)
-  end
-end
