27b3565a6efdd19fa6dbb1a57176d59e208fcefc
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2015-03-01 15:10:06 +0300

    Used ability to handle authorization in bugs_controller. Used rocket-hash instead of new syntax in bugs_controller.

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -4,24 +4,26 @@ class BugsController < ApplicationController
 
   def index
     if params[:for].nil? || params[:for] == 'author'
-      @bugs = Bug.for_author(@user)
+      @bugs = Bug.accessible_by(current_ability).for_author(@user)
     else
-      @bugs = Bug.for_reporter(@user)
+      @bugs = Bug.accessible_by(current_ability).for_reporter(@user)
     end
-    @bugs = @bugs.where(post_id: params[:post_id]) if params[:post_id].present?
+    @bugs = @bugs.where(:post_id => params[:post_id]) if params[:post_id].present?
     @bugs = @bugs.in_order
     @bugs = @bugs.page(params[:page]).per(params[:per])
   end
 
   def show
-    @bug = Bug.find(params[:id])
+    @bug = Bug.accessible_by(current_ability).find(params[:id])
+    authorize! :show, @bug
     @comments = @bug.comments
     @comments = @comments.in_order
     @comments = @comments.page(params[:page]).per(params[:per])
   end
 
   def fix
-    @bug = Bug.for_author(@user).find(params[:id])
+    @bug = Bug.accessible_by(current_ability).for_author(@user).find(params[:id])
+    authorize! :update, @bug
     if @bug.fix
       flash[:notice] = I18n.t('bugs.fix.success')
     else
@@ -31,7 +33,8 @@ class BugsController < ApplicationController
   end
 
   def reject
-    @bug = Bug.for_author(@user).find(params[:id])
+    @bug = Bug.accessible_by(current_ability).for_author(@user).find(params[:id])
+    authorize! :update, @bug
     if @bug.reject
       flash[:notice] = I18n.t('bugs.reject.success')
     else
@@ -41,8 +44,9 @@ class BugsController < ApplicationController
   end
     
   def create
-    @post = Post.published_only.find(params[:post_id])
-    @bug = @post.bugs.build(reporter_id: @user.id, note: params[:note])
+    authorize! :create, Bug
+    @post = Post.accessible_by(current_ability).find(params[:post_id])
+    @bug = @post.bugs.build(:reporter_id => @user.id, :note => params[:note])
     @bug.fragment = params[:fragment] if params[:fragment].present?
     if @bug.save
       flash[:notice] = I18n.t('bugs.create.success')
