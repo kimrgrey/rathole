af9f0b6f689f2933b1c21f429a3561e6ff4e7663
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2015-07-12 03:50:44 +0300

    Fixed migration for parents of comments

--- db/migrate/20150711234044_fill_comments_parents.rb
+++ db/migrate/20150711234044_fill_comments_parents.rb
@@ -1,6 +1,6 @@
 class FillCommentsParents < ActiveRecord::Migration
   def change
-    execute "UPDATE comments SET parent_type = 'Post', parent_id = post_id WHERE type = 'Comment'"
-    execute "UPDATE comments SET parent_type = 'Bug', parent_id = bug_id WHERE type = 'Comment'"
+    execute "UPDATE comments SET parent_type = 'Post', parent_id = post_id WHERE post_id IS NOT NULL"
+    execute "UPDATE comments SET parent_type = 'Bug', parent_id = bug_id WHERE bug_id IS NOT NULL"
   end
 end
