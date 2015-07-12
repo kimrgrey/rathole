5458ca7689df69794dd2c766353d578ec17b6925
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2015-07-12 03:51:35 +0300

    Bump schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150712002714) do
+ActiveRecord::Schema.define(version: 20150712004916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,11 +48,9 @@ ActiveRecord::Schema.define(version: 20150712002714) do
   create_table "comments", force: true do |t|
     t.text     "body"
     t.integer  "user_id"
-    t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "body_html"
-    t.integer  "bug_id"
     t.datetime "body_updated_at"
     t.datetime "deleted_at"
     t.integer  "parent_id",       null: false
