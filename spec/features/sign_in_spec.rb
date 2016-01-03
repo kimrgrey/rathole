878c20dead309997739e9b799a20da7c908c3e27
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2016-01-03 22:17:53 +0300

    Added specs for sing in with wrong email and password

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
-describe "registered and confirmed user", :type => :feature, :js => true do
+describe "sign in", :type => :feature, :js => true do
   fixtures :users
 
-  it "should be able to sign in" do
+  it "should redirect to events page for correct email and password" do
     visit '/users/sign_in'
 
     fill_in I18n.t("activerecord.attributes.user.email"), :with => "tom@example.com"
@@ -11,5 +11,27 @@ describe "registered and confirmed user", :type => :feature, :js => true do
     click_button I18n.t("devise.sessions.new.submit")
 
     expect(page.current_path).to eq("/user/events")
+  end
+
+  it "should show error message for wrong email" do
+    visit '/users/sign_in'
+
+    fill_in I18n.t("activerecord.attributes.user.email"), :with => "wrongemail@example.com"
+    fill_in I18n.t("activerecord.attributes.user.password"), :with => "password"
+    click_button I18n.t("devise.sessions.new.submit")
+
+    expect(page.current_path).to eq("/users/sign_in")
+    expect(page).to have_content(I18n.t("devise.failure.invalid"))
+  end
+
+  it "should show error message for wrong password" do
+    visit '/users/sign_in'
+
+    fill_in I18n.t("activerecord.attributes.user.email"), :with => "tom@example.com"
+    fill_in I18n.t("activerecord.attributes.user.password"), :with => "wrongpassword"
+    click_button I18n.t("devise.sessions.new.submit")
+
+    expect(page.current_path).to eq("/users/sign_in")
+    expect(page).to have_content(I18n.t("devise.failure.invalid"))
   end
 end
