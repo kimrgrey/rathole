c10f09a5fdca75a8c153091823e50a30ead4a3b5
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2016-01-04 00:39:13 +0300

    Added specs which checks that not admin user is not able to access admin area

--- spec/features/signed_in_user_spec.rb
+++ spec/features/signed_in_user_spec.rb
@@ -1,25 +1,45 @@
 require "rails_helper"
 
-describe "signed in user", :type => :feature, :js => true do
+describe "signed in user", :type => :feature do
   fixtures :users
 
-  before do
-    sign_in_as :tom
+  describe "anyway" do
+    before do
+      sign_in_as :tom
+    end
+
+    it "should not be able to access sign in page" do
+      visit '/user/events'
+      visit '/users/sign_in'
+
+      expect(page.current_path).to eq("/user/events")
+      expect(page).to have_content I18n.t("devise.failure.already_authenticated")
+    end
+
+    it "should not be able to access sign up page" do
+      visit '/user/events'
+      visit '/users/new'
+
+      expect(page.current_path).to eq("/user/events")
+      expect(page).to have_content I18n.t("devise.failure.already_authenticated")
+    end
   end
 
-  it "should not be able to access sign in page" do
-    visit '/user/events'
-    visit '/users/sign_in'
+  describe "who is not admin" do
+    before do
+      sign_in_as :tom
+    end
 
-    expect(page.current_path).to eq("/user/events")
-    expect(page).to have_content I18n.t("devise.failure.already_authenticated")
-  end
+    it "should not be able to know about an amdin dashboard" do
+      expect{
+        visit "/admin"
+      }.to raise_error ActionController::RoutingError
+    end
 
-  it "should not be able to access sign up page" do
-    visit '/user/events'
-    visit '/users/new'
-
-    expect(page.current_path).to eq("/user/events")
-    expect(page).to have_content I18n.t("devise.failure.already_authenticated")
+    it "should not be able to know about an queues monitor" do
+      expect{
+        visit "/admin/jobs"
+      }.to raise_error ActionController::RoutingError
+    end
   end
 end
