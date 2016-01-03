82f3afe10fa43054f96a9ef6be8ca04fa6248b8b
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2016-01-03 18:37:21 +0300

    Updated rails env configs to run application.

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,9 @@ module Rathole
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.time_zone = 'Moscow'
     config.after_initialize do
-      ActionView::Base.sanitized_allowed_attributes += ['rel', 'target', 'style']
+      ActionView::Base.sanitized_allowed_attributes = ['rel', 'target', 'style']
     end
     config.autoload_paths << "#{config.root}/app/validators"
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -9,6 +9,7 @@ Rathole::Application.configure do
   config.action_dispatch.show_exceptions = false
   config.action_controller.allow_forgery_protection = false
   config.active_support.deprecation = :stderr
-  config.action_mailer.delivery_method = :test    
+  config.action_mailer.delivery_method = :test
   config.action_mailer.default_url_options = { :host => 'rathole.dev' }
+  config.active_support.test_order = :sorted
 end
