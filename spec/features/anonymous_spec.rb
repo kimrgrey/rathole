c3b8f318be5e4ef11e6e692cc8d4874e1ecabadb
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2016-01-03 21:38:25 +0300

    Just replace is with should in anonymous_spec

--- spec/features/anonymous_spec.rb
+++ spec/features/anonymous_spec.rb
@@ -1,14 +1,14 @@
 require "rails_helper"
 
 describe "anonymous user", :type => :feature, :js => true do
-  it "is able to visit sign in page" do
+  it "should able to visit sign in page" do
     visit '/users/sign_in'
     within('h2') do
       expect(page).to have_content I18n.t("devise.sessions.new.page_header")
     end
   end
 
-  it "is able to visit sign up page" do
+  it "should able to visit sign up page" do
     visit '/users/new'
     within('h2') do
       expect(page).to have_content I18n.t("devise.registrations.new.page_header")
