73f28058f10f94ff818cb13fb8de4eeab9d653af
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2016-01-03 21:36:26 +0300

    Small test helper changes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,21 @@
 RSpec.configure do |config|
+  config.order = :random
+
+  config.profile_examples = 0
+
   config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
   config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
     mocks.verify_partial_doubles = true
   end
 
   config.filter_run :focus
   config.run_all_when_everything_filtered = true
   config.disable_monkey_patching!
-  config.order = :random
   config.expose_dsl_globally = true
 
   Kernel.srand config.seed
