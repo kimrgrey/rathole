308aec4fd6669cbf01a1f78f99442ac5228d22ad
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2017-02-11 15:47:07 +0300

    Disabled stickers distribution

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 set :output, '/home/rathole/rathole/shared/log/whenever.log'
 
 every 1.day, at: '20:30 pm', roles: [:app] do
-  rake 'stickers:distribute'
+  # rake 'stickers:distribute'
   rake 'sitemap:refresh'
 end
